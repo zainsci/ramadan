863c2b63a3370e06ccae9fd4826c00873eda72d3
zainsci <60652827+zainsci@users.noreply.github.com>
2021-04-13 22:10:11 +0500

    Add Compoenst to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import "./styles/styles.css";
+import "./styles/normalize.css";
+import Hero from "./components/hero";
+import Header from "./components/header";
+import SocialIcons from "./components/social-icons";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div
+      className="App"
+      style={{
+        "background-image":
+          "url(" + process.env.PUBLIC_URL + "/assets/pattern.png)",
+      }}
+    >
+      <Header />
+      <Hero />
+      <SocialIcons />
+    </div>
+  );
 }
 
 export default App;
